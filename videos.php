2d63c58aa01f38671114f8e5ca8b3b8fe19b0405
MonsterLuni <monsterluni.specific@gmail.com>
2023-08-20 20:23:03 +0200

    Add Database Connection

--- videos.php
+++ videos.php
@@ -12,10 +12,21 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $conn = new mysqli($servername, $username, $password);
+    $database = "ArchiveOfMyself";
+    $conn = new mysqli($servername, $username, $password, $database);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
+    // Url, Likes, Dislikes, Comments
+    $query = $conn->query("SELECT * FROM videos");
+    $videos = $query->fetch_all();
+    foreach ($videos as $video) {
+        echo "<p>URL: $video[0]</p>";
+        echo "<p>Likes: $video[1]</p>";
+        echo "<p>Dislikes: $video[2]</p>";
+        echo "<p>Comments: $video[3]</p>";
+        //var_dump($video[0]);
+    }
+
     ?>
 </div>
