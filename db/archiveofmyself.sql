c17748971ed9494ccf40ab5d363963e93f0be722
MonsterLuni <monsterluni@gmail.com>
2023-08-21 15:53:43 +0200

    database new

--- db/archiveofmyself.sql
+++ db/archiveofmyself.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 20. Aug 2023 um 20:56
--- Server-Version: 10.4.27-MariaDB
--- PHP-Version: 8.1.12
+-- Erstellungszeit: 21. Aug 2023 um 15:53
+-- Server-Version: 10.4.28-MariaDB
+-- PHP-Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `comments`
+--
+
+CREATE TABLE `comments` (
+  `text` varchar(250) NOT NULL,
+  `likes` int(11) NOT NULL,
+  `dislikes` int(11) NOT NULL,
+  `comments` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `video_fk` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Daten für Tabelle `comments`
+--
+
+INSERT INTO `comments` (`text`, `likes`, `dislikes`, `comments`, `id`, `video_fk`) VALUES
+('Ich fand dieses Video weltverändernd', 53, 18, 3, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(50) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `videos`
 --
 
@@ -31,21 +66,36 @@ CREATE TABLE `videos` (
   `url` varchar(50) NOT NULL,
   `likes` int(11) NOT NULL,
   `dislikes` int(11) NOT NULL,
-  `comments` int(11) NOT NULL,
+  `text` varchar(250) NOT NULL,
+  `description` varchar(250) NOT NULL,
   `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Daten für Tabelle `videos`
 --
 
-INSERT INTO `videos` (`url`, `likes`, `dislikes`, `comments`, `id`) VALUES
-('Luz.mp4', 12, 28, 124, 1),
-('Hunter.mp4', 0, 0, 0, 2);
+INSERT INTO `videos` (`url`, `likes`, `dislikes`, `text`, `description`, `id`) VALUES
+('Luz.mp4', 42, 23, 'Dies ist der Text', 'Dies ist die Beschreibung', 1),
+('Hunteer.mp4', 7654, 87, 'text 2', 'Beschreibung 2', 2),
+('Jeesse.mp4', 7674564, 2847, 'text 3', 'Beschreibung 3', 3);
 
 --
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `video_fk` (`video_fk`);
+
+--
+-- Indizes für die Tabelle `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indizes für die Tabelle `videos`
@@ -58,10 +108,32 @@ ALTER TABLE `videos`
 --
 
 --
+-- AUTO_INCREMENT für Tabelle `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT für Tabelle `videos`
 --
 ALTER TABLE `videos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`video_fk`) REFERENCES `videos` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
